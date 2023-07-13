7cf835730a43c704be93f54360f2e32019d69313
KDHun <201901137@daiict.ac.in>
2023-07-13 14:13:38 +0530

    rander deployment setup

--- src/Components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import cardclass from "./Card.module.css";
-
-const Card = (props) => {
-  const classes = cardclass.Card +  ' ' + props.className;
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default Card;
